da15b886c30811237d6849d5e0722fb94081ca5c
Yancy Garret <yancygarret12@gmail.com>
2024-05-23 21:24:17 +0800

    commit

--- src/components/CategoryGallery.tsx
+++ src/components/CategoryGallery.tsx
@@ -10,7 +10,7 @@ const CategoryGallery = () => {
 
     return (
         <div className="w-4/5">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div className="flex flex-wrap gap-4">
                 {filteredProducts.map((product) => (
                     <div key={product.id} className="p-4 border rounded-lg">
                         <img
