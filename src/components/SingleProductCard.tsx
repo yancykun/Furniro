f5e3e7241b0bfc792a2c2d780be6670e101122e2
Yancy Garret <yancygarret12@gmail.com>
2024-05-30 13:47:35 +0800

    commit

--- src/components/SingleProductCard.tsx
+++ src/components/SingleProductCard.tsx
@@ -13,32 +13,35 @@ const SingleProductCard = () => {
     }
 
     return (
-        <div className="flex flex-col justify-center md:flex-row lg:px-[99px] pt-[33px] gap-4 w-full">
-            <div className="w-full">
+        <div className="flex flex-col md:flex-row justify-center gap-10 md:gap-20 px-[2rem] lg:px-[99px] pt-[33px] w-full">
+            <div className="flex justify-center md:justify-end w-full">
                 <img
-                    className="lg:w-[423px] lg:h-[500px]"
+                    className="w-[350px] h-[400px]"
                     src={product.image}
                     alt={product.name}
                 />
             </div>
-            <div className="flex flex-col w-full">
+
+            <div className="flex flex-col items-center md:items-start w-full">
                 <div className="border-b border-color-6/50 pb-10">
                     <p className="font-poppins text-[2.63rem]">
                         {product.name}
                     </p>
-                    <p className="font-poppins text-[1.5rem] font-medium text-color-6 pb-1">
-                        {product.price}
-                    </p>
-                    <div className="flex items-center gap-4 pb-2">
-                        <StarRating rating={product.rating} />
-                        <p className="font-poppins text-base text-color-6">
-                            ({product.popularity})
+                    <div className="flex md:flex-col justify-between">
+                        <p className="font-poppins text-[1.5rem] font-medium text-color-6 pb-1">
+                            $ {product.price}
                         </p>
+                        <div className="flex items-center gap-4 pb-2">
+                            <StarRating rating={product.rating} />
+                            <p className="font-poppins text-base text-color-6">
+                                ({product.popularity})
+                            </p>
+                        </div>
                     </div>
                     <p className="font-poppins text-[0.8125rem] text-color-7 font-medium max-w-[424px] pb-6">
                         {product.description}
                     </p>
-                    <div className="flex gap-5 items-center">
+                    <div className="flex max-sm:flex-col gap-5 items-center">
                         <div className="flex items-center justify-center gap-6 border border-color-6 h-[55px] w-[120px] rounded-lg">
                             <button className="font-poppins text-color-7">
                                 -
