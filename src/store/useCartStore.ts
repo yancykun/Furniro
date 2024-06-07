fa2cc2df20ccdb2ab927145e94db22a54e4f25b0
Yancy Garret <yancygarret12@gmail.com>
2024-06-07 21:53:11 +0800

    commit

--- src/store/useCartStore.ts
+++ src/store/useCartStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 type CartItem = {
   id: string;
@@ -16,55 +17,66 @@ type CartState = {
   itemCount: number;
 };
 
-export const useCartStore = create<CartState>((set, get) => ({
-  cart: [],
-  itemCount: 0,
+export const useCartStore = create<CartState>()(
+  persist(
+    (set, get) => ({
+      cart: [],
+      itemCount: 0,
 
-  addToCart: (product: CartItem) => {
-    set((state) => {
-      const existingProduct = state.cart.find((item) => item.id === product.id);
+      addToCart: (product: CartItem) => {
+        set((state) => {
+          const existingProduct = state.cart.find(
+            (item) => item.id === product.id,
+          );
 
-      if (existingProduct) {
-        return {
-          cart: state.cart.map((item) =>
-            item.id === product.id
-              ? {
-                  ...item,
-                  quantity: item.quantity + product.quantity,
-                }
-              : item,
-          ),
-          itemCount:
-            state.cart.reduce((count, item) => count + item.quantity, 0) +
-            product.quantity,
-        };
-      }
+          if (existingProduct) {
+            return {
+              cart: state.cart.map((item) =>
+                item.id === product.id
+                  ? {
+                      ...item,
+                      quantity: item.quantity + product.quantity,
+                    }
+                  : item,
+              ),
+              itemCount:
+                state.cart.reduce((count, item) => count + item.quantity, 0) +
+                product.quantity,
+            };
+          }
 
-      return {
-        cart: [...state.cart, product],
-        itemCount:
-          state.cart.reduce((count, item) => count + item.quantity, 0) +
-          product.quantity,
-      };
-    });
-  },
+          return {
+            cart: [...state.cart, product],
+            itemCount:
+              state.cart.reduce((count, item) => count + item.quantity, 0) +
+              product.quantity,
+          };
+        });
+      },
 
-  removeFromCart: (id: string) => {
-    set((state) => {
-      const removedItem = state.cart.find((item) => item.id === id);
-      const newCart = state.cart.filter((item) => item.id !== id);
+      removeFromCart: (id: string) => {
+        set((state) => {
+          const removedItem = state.cart.find((item) => item.id === id);
+          const newCart = state.cart.filter((item) => item.id !== id);
 
-      return {
-        cart: newCart,
-        itemCount: state.itemCount - (removedItem ? removedItem.quantity : 0),
-      };
-    });
-  },
+          return {
+            cart: newCart,
+            itemCount:
+              state.itemCount - (removedItem ? removedItem.quantity : 0),
+          };
+        });
+      },
 
-  getTotalPrice: () => {
-    return get().cart.reduce(
-      (total, item) => total + item.price * item.quantity,
-      0,
-    );
-  },
-}));
+      getTotalPrice: () => {
+        return get().cart.reduce(
+          (total, item) => total + item.price * item.quantity,
+          0,
+        );
+      },
+    }),
+    {
+      name: "cart-storage", // name of the item in the storage (must be unique)
+      getStorage: () => localStorage, // (optional) by default, 'localStorage' is used
+    },
+  ),
+);
