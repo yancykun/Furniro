1ac9aef028f0ba313f3a23f2c805712ea7d4902d
Yancy Garret <yancygarret12@gmail.com>
2024-06-08 23:26:23 +0800

    commit

--- src/components/Section/Footer/Footer.tsx
+++ src/components/Section/Footer/Footer.tsx
@@ -73,7 +73,7 @@ const Footer = () => {
       </div>
 
       <p className="px-[20px] py-8 text-center font-poppins font-semibold lg:px-[100px] lg:text-start">
-        2024 furino. All rights reverved
+        2024 furino. All rights reserved
       </p>
     </Section>
   );
