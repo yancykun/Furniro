9f3be9fbc7e8a81162a0d110af52b41bf6ac1c14
Yancy Garret <yancygarret12@gmail.com>
2024-06-13 21:21:17 +0800

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,12 @@ import BillingPage from "./pages/BillingPage";
 import Header from "./components/Section/Header/Header";
 import CartSidebar from "./components/Features/Cart/CartSidebar";
 import ScrollToTop from "./components/Features/ScrollToTop/ScrollToTop";
+import ProfileSidebar from "./components/Features/Profile/ProfileSidebar";
 import { useCartSidebarStore } from "./store/useCartSidebarStore";
+import { useProfileSidebarStore } from "./store/useProfileSidebarStore";
 const App = () => {
   const openCart = useCartSidebarStore((state) => state.openCart);
+  const openProfile = useProfileSidebarStore((state) => state.openProfile);
 
   return (
     <>
@@ -33,6 +36,14 @@ const App = () => {
         <div className="fixed right-0 top-0 z-50 h-[550px] w-[80%] sm:w-[417px]">
           <CartSidebar />
         </div>
+      )}
+      {openProfile && (
+        <>
+          <div className="fixed inset-0 z-40 bg-color-7/40"></div>
+          <div className="fixed right-0 top-0 z-50 h-[250px] w-[80%] sm:w-[350px]">
+            <ProfileSidebar />
+          </div>
+        </>
       )}
     </>
   );
--- src/components/Features/Account/AccountSidebar.tsx
+++ src/components/Features/Account/AccountSidebar.tsx
@@ -1,31 +1,47 @@
 import { CgProfile } from "react-icons/cg";
 import Button from "../../UI/Button";
 import { Link } from "react-router-dom";
+import { useProfileSidebarStore } from "../../../store/useProfileSidebarStore";
+import { useState } from "react";
 
 const AccountSidebar = () => {
+  const [isLoggedIn] = useState(false);
+  const closeProfileSidebar = useProfileSidebarStore(
+    (state) => state.closeProfileSidebar,
+  );
+
   return (
-    <div className="grid">
-      <div className="flex items-center gap-2 sm:gap-4">
-        <CgProfile className="cursor-pointer" size={35} />
-        <p className="font-poppins text-sm text-color-6 sm:text-base">
-          yancygarret@gmail.com
-        </p>
-      </div>
-      <Link to="/cart">
-        <Button
-          white
-          className="mt-4 h-[30px] w-[120px] rounded-[50px] font-poppins text-xs font-medium"
-        >
-          Cart
-        </Button>
-      </Link>
-      <Button
-        white
-        className="mt-2 h-[30px] w-[120px] rounded-[50px] font-poppins text-xs font-medium"
-      >
-        Logout
-      </Button>
-    </div>
+    <>
+      {isLoggedIn ? (
+        <div className="grid">
+          <div className="flex items-center gap-2 sm:gap-4">
+            <CgProfile className="cursor-pointer" size={35} />
+            <p className="font-poppins text-sm text-color-6 sm:text-base">
+              yancygarret@gmail.com
+            </p>
+          </div>
+          <Link to="/cart">
+            <Button
+              white
+              onClick={closeProfileSidebar}
+              className="mt-4 h-[30px] w-[120px] rounded-[50px] font-poppins text-xs font-medium"
+            >
+              Cart
+            </Button>
+          </Link>
+          <Button
+            white
+            className="mt-2 h-[30px] w-[120px] rounded-[50px] font-poppins text-xs font-medium"
+          >
+            Logout
+          </Button>
+        </div>
+      ) : (
+        <div>
+          <p>You are not logged in.</p>
+        </div>
+      )}
+    </>
   );
 };
 
