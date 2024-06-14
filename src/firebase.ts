9c4ad888c9ad5d0064d6637995510726fb3fb860
Yancy Garret <yancygarret12@gmail.com>
2024-06-14 19:57:42 +0800

    commit

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+console.log("API Key:", import.meta.env.VITE_FIREBASE_API_KEY);
+console.log("Auth Domain:", import.meta.env.VITE_FIREBASE_AUTH_DOMAIN);
+console.log("Project ID:", import.meta.env.VITE_FIREBASE_PROJECT_ID);
+console.log("Storage Bucket:", import.meta.env.VITE_FIREBASE_STORAGE_BUCKET);
+console.log(
+  "Messaging Sender ID:",
+  import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+);
+console.log("App ID:", import.meta.env.VITE_FIREBASE_APP_ID);
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
