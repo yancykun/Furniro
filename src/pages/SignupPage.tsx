067a2070a54c5873af170dfbb3b197350d03da05
Yancy Garret <yancygarret12@gmail.com>
2024-06-15 20:42:20 +0800

    commit

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -3,7 +3,7 @@ import Section from "../components/Layout/Section";
 
 const SignupPage = () => {
   return (
-    <Section className="mt-20">
+    <Section className="mt-21">
       <SignupForm />
     </Section>
   );
