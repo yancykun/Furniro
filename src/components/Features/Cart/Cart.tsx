0bb746cc12d29e50c0596ec3b41ee855160cd317
Yancy Garret <yancygarret12@gmail.com>
2024-06-26 16:57:26 +0800

    cart total functionality debugging

--- src/components/Features/Cart/Cart.tsx
+++ src/components/Features/Cart/Cart.tsx
@@ -1,12 +1,10 @@
 import CartTable from "../Cart/CartTable";
 import Section from "../../Layout/Section";
-import CartTotals from "./CartTotals";
 
 const Cart = () => {
   return (
-    <Section className="flex flex-col items-center justify-center gap-8 sm:px-8 lg:flex-row lg:items-start lg:px-[50px]">
+    <Section>
       <CartTable />
-      <CartTotals />
     </Section>
   );
 };
