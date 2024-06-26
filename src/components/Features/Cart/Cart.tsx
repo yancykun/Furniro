ad522a35448e85796f08737a9d5939a876a97956
Yancy Garret <yancygarret12@gmail.com>
2024-06-26 19:46:26 +0800

    commit

--- src/components/Features/Cart/Cart.tsx
+++ src/components/Features/Cart/Cart.tsx
@@ -1,41 +1,7 @@
-import { useEffect } from "react";
 import CartTable from "../Cart/CartTable";
 import Section from "../../Layout/Section";
-import { useCartStore } from "../../../store/useCartStore";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
-
-const fetchCartItems = async () => {
-  const db = getFirestore();
-  const cartCollection = collection(db, "cartItems");
-  const cartSnapshot = await getDocs(cartCollection);
-  const cartList = cartSnapshot.docs.map((doc) => {
-    const data = doc.data();
-    return {
-      id: doc.id,
-      image: data.image,
-      title: data.title,
-      quantity: data.quantity,
-      price: parseFloat(data.price), // Ensure price is a number
-    };
-  });
-  return cartList;
-};
 
 const Cart = () => {
-  const { addToCart } = useCartStore();
-
-  useEffect(() => {
-    // Clear specific item from local storage
-    localStorage.removeItem("cart-storage");
-
-    const loadCartItems = async () => {
-      const items = await fetchCartItems();
-      items.forEach((item) => addToCart(item));
-    };
-
-    loadCartItems();
-  }, [addToCart]);
-
   return (
     <Section>
       <CartTable />
--- src/store/useCartStore.ts
+++ src/store/useCartStore.ts
@@ -1,5 +1,4 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
 
 type CartItem = {
   id: string;
@@ -17,70 +16,59 @@ type CartState = {
   itemCount: number;
 };
 
-export const useCartStore = create<CartState>()(
-  persist(
-    (set, get) => ({
-      cart: [],
-      itemCount: 0,
+export const useCartStore = create<CartState>()((set, get) => ({
+  cart: [],
+  itemCount: 0,
 
-      addToCart: (product: CartItem) => {
-        if (typeof product.price !== "number") {
-          throw new Error("Price must be a number");
-        }
+  addToCart: (product: CartItem) => {
+    if (typeof product.price !== "number") {
+      throw new Error("Price must be a number");
+    }
 
-        set((state) => {
-          const existingProduct = state.cart.find(
-            (item) => item.id === product.id,
-          );
+    set((state) => {
+      const existingProduct = state.cart.find((item) => item.id === product.id);
 
-          if (existingProduct) {
-            return {
-              cart: state.cart.map((item) =>
-                item.id === product.id
-                  ? {
-                      ...item,
-                      quantity: item.quantity + product.quantity,
-                    }
-                  : item,
-              ),
-              itemCount:
-                state.cart.reduce((count, item) => count + item.quantity, 0) +
-                product.quantity,
-            };
-          }
+      if (existingProduct) {
+        return {
+          cart: state.cart.map((item) =>
+            item.id === product.id
+              ? {
+                  ...item,
+                  quantity: item.quantity + product.quantity,
+                }
+              : item,
+          ),
+          itemCount:
+            state.cart.reduce((count, item) => count + item.quantity, 0) +
+            product.quantity,
+        };
+      }
 
-          return {
-            cart: [...state.cart, product],
-            itemCount:
-              state.cart.reduce((count, item) => count + item.quantity, 0) +
-              product.quantity,
-          };
-        });
-      },
+      return {
+        cart: [...state.cart, product],
+        itemCount:
+          state.cart.reduce((count, item) => count + item.quantity, 0) +
+          product.quantity,
+      };
+    });
+  },
 
-      removeFromCart: (id: string) => {
-        set((state) => {
-          const removedItem = state.cart.find((item) => item.id === id);
-          const newCart = state.cart.filter((item) => item.id !== id);
+  removeFromCart: (id: string) => {
+    set((state) => {
+      const removedItem = state.cart.find((item) => item.id === id);
+      const newCart = state.cart.filter((item) => item.id !== id);
 
-          return {
-            cart: newCart,
-            itemCount:
-              state.itemCount - (removedItem ? removedItem.quantity : 0),
-          };
-        });
-      },
+      return {
+        cart: newCart,
+        itemCount: state.itemCount - (removedItem ? removedItem.quantity : 0),
+      };
+    });
+  },
 
-      getTotalPrice: () => {
-        return get().cart.reduce(
-          (total, item) => total + item.price * item.quantity,
-          0,
-        );
-      },
-    }),
-    {
-      name: "cart-storage", // name of the item in the storage (must be unique)
-      getStorage: () => localStorage, // (optional) by default, 'localStorage' is used
-    },
-  ),
-);
+  getTotalPrice: () => {
+    return get().cart.reduce(
+      (total, item) => total + item.price * item.quantity,
+      0,
+    );
+  },
+}));
